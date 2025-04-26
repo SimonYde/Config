bb414b20534331a802a1ec30bed9ce3090943b72
Simon Yde <git@simonyde.com>
2025-04-26 14:50:30 +0200

    refactor(home-manager): separate large shell program configs to their own files

---
+++ common/home-manager/shell/git.nix
@@ -0,0 +1,110 @@
+_:
+
+{
+  programs.git = {
+    userName = "Simon Yde";
+    userEmail = "git@simonyde.com";
+
+    aliases = {
+      a = "add";
+      br = "branch";
+      sw = "switch";
+
+      cc = "commit -v";
+      ca = "commit --all -v";
+
+      d = "diff";
+      st = "status -s";
+
+      ls = ''log --pretty=format:"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]" --decorate'';
+      ll = ''log --pretty=format:"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]" --decorate --numstat'';
+
+      filelog = "log -u";
+      fl = "log -u";
+
+      dl = "!git ll -1";
+
+      dr = ''!f() { git diff "$1"^.."$1"; }; f'';
+
+      fixup = "commit --fixup HEAD";
+      fix = "rebase -i --autosquash";
+
+      rhh = "reset --hard HEAD";
+      alias = "config --get-regexp ^alias\\.";
+    };
+
+    extraConfig = {
+      init.defaultBranch = "master";
+
+      merge.conflictStyle = "zdiff3";
+      push = {
+        default = "current";
+        autoSetupRemote = true;
+      };
+
+      # Rebase
+      branch.autosetuprebase = "always";
+      pull.rebase = "merges";
+      rebase.autostash = true;
+      rebase.updateRefs = true;
+
+      commit.verbose = true;
+      rerere.enabled = true;
+
+      column.ui = "auto";
+      color.ui = "auto";
+
+      diff = {
+        colorMoved = "default";
+        submodule = "log";
+        algorithm = "histogram";
+      };
+
+      branch.sort = "-committerdate";
+      tag.sort = "-taggerdate";
+      log.date = "iso";
+
+      # Signing commits
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+
+      fetch = {
+        prune = true;
+        recurseSubmodules = true;
+      };
+
+      # Submodules
+      submodule.recurse = true;
+      push.recurseSubmodules = false;
+      status.submoduleSummary = true;
+
+      url = {
+        "ssh://git@codeberg.org/".insteadOf = "cb:";
+        "ssh://git@github.com/".insteadOf = "gh:";
+        "ssh://git@gitlab.com/".insteadOf = "gl:";
+        "ssh://git@gitlab.au.dk/".insteadOf = "au:";
+      };
+    };
+
+    ignores = [
+      "**/.idea"
+      "**/.metals"
+      "**/_build"
+      "**/elm-stuff"
+      "**/node_modules"
+      "**/target"
+      "*.log"
+      "*.sync-conflict*"
+      ".direnv"
+      ".env"
+      ".envrc"
+      "**/.mypy_cache"
+      "**/__pycache__"
+      "**/.ruff_cache"
+      "**/.stfolder"
+      "**/.stignore"
+      ".vscode"
+    ];
+  };
+}
---
+++ common/home-manager/shell/neovim.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, ... }:
+
+{
+  programs.neovim = {
+    package = pkgs.neovim;
+    defaultEditor = true;
+
+    vimAlias = true;
+    viAlias = true;
+
+    withRuby = false;
+    withNodeJs = false;
+    withPython3 = false;
+
+    plugins =
+      with pkgs.vimPlugins;
+      [
+        # ----- Workflow -----
+        nvim-autopairs
+        mini-nvim
+        snacks-nvim
+        vim-sleuth
+        undotree
+        friendly-snippets
+
+        obsidian-nvim
+
+        # ----- UI -----
+        which-key-nvim
+        nvim-treesitter
+        tip-vim
+      ]
+      ++ config.lib.meta.lazyNeovimPlugins [
+        # ----- Completion -----
+        blink-cmp
+
+        # ----- Workflow -----
+        nvim-lspconfig
+        conform-nvim
+        trouble-nvim
+        diffview-nvim
+        neogit
+        todo-comments-nvim
+        img-clip-nvim
+        nvim-ufo
+
+        nvim-dap
+        nvim-dap-ui
+
+        yazi-nvim
+
+        # ----- UI -----
+        lspsaga-nvim
+        render-markdown-nvim
+        nvim-treesitter-textobjects
+        nvim-treesitter-context
+        rainbow-delimiters-nvim
+      ];
+
+    extraPackages = with pkgs; [
+      ghostscript # snacks.nvim PDF rendering
+    ];
+  };
+}
