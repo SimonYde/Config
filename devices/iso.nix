e9479e5d4764972f5d9c087187999a664a02371a
Simon Yde <git@simonyde.com>
2025-05-23 16:18:22 +0200

    reinstall perdix with ZFS

--- devices/iso.nix
+++ devices/iso.nix
@@ -2,16 +2,30 @@
   lib,
   modulesPath,
   pkgs,
+  config,
   ...
 }:
 
+let
+  zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+    && (builtins.tryEval kernelPackages).success
+    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+  ) pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in
 {
   imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
   ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = latestKernelPackage;
     supportedFilesystems.zfs = lib.mkForce true;
   };
 }
