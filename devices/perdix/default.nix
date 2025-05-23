735edb20488f29694854fab5cf270d52bb8134f1
Simon Yde <git@simonyde.com>
2025-05-23 16:18:22 +0200

    reinstall perdix with ZFS

--- devices/perdix/default.nix
+++ devices/perdix/default.nix
@@ -32,7 +32,6 @@
     };
   };
 
-  services.logind.lidSwitch = "ignore";
   console = {
     earlySetup = true;
     useXkbConfig = true;
@@ -81,56 +80,19 @@
   };
 
   services = {
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    logind.lidSwitch = "ignore";
+
     tailscale = {
       enable = true;
       authKeyFile = config.age.secrets.tailscale.path;
     };
 
     syncthing.enable = true;
-
-    kanata = {
-      enable = true;
-
-      keyboards.laptop-keyboard = {
-        config = # lisp
-          ''
-            (defsrc
-              caps a s d f j k l ;
-            )
-            (defvar
-              tap-time 250
-              hold-time 250
-              shift-time 180
-            )
-
-            (defalias
-              a (tap-hold $tap-time $hold-time a lmet)
-              s (tap-hold $tap-time $hold-time s lalt)
-              d (tap-hold $tap-time $hold-time d lctl)
-              f (tap-hold $shift-time $shift-time f lsft)
-
-              j (tap-hold $shift-time $shift-time j rsft)
-              k (tap-hold $tap-time $hold-time k lctl)
-              l (tap-hold $tap-time $hold-time l lalt)
-              ; (tap-hold $tap-time $hold-time ; rmet)
-            )
-
-            (deflayer default-layer
-              esc @a @s @d @f @j @k @l @;
-            )
-          '';
-        extraDefCfg = "process-unmapped-keys yes";
-        devices = [
-          "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-        ];
-      };
-    };
-
-    xserver.xkb = {
-      layout = "us,dk";
-      variant = "colemak_dh,";
-      options = "caps:escape,grp:rctrl_toggle";
-    };
   };
 
   networking.wireguard.enable = true;
