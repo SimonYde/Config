99a67c6d6eca6093c68cd18b91eb127f69538cf2
Simon Yde <git@simonyde.com>
2026-01-21 17:12:12 +0100

    refactor(audio-production): don't have lsp-plugins directly on path

--- common/home-manager/audio-production.nix
+++ common/home-manager/audio-production.nix
@@ -2,12 +2,9 @@
   pkgs,
   ...
 }:
-let
-in
 {
   home.packages = with pkgs; [
     jamesdsp
-    lsp-plugins
     yabridge
     yabridgectl
 
@@ -15,4 +12,6 @@ in
     reaper-reapack-extension
     reaper-sws-extension
   ];
+
+  home.file.".lv2/lsp-plugins.lv2".source = "${pkgs.lsp-plugins}/lib/lv2/lsp-plugins.lv2";
 }
