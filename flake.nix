209108385cde00349f61720a930ac43bb9723059
Simon Yde <git@simonyde.com>
2026-06-20 23:37:57 +0200

    update lanzaboote to fix module build

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:nix-community/lanzaboote/2a9c6ba61f2e1bd6eaf4e1e12aca77699bf7ea95";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         pre-commit.follows = "pre-commit-hooks";
